2bf56a6ee44032d08297e75363425171eaebccd6
daradan <daradankz@gmail.com>
2022-10-03 11:02:44 +0600

    exclude - self.data use only local variable - data new function for get categories id & name change logic on send to telegram

--- main.py
+++ main.py
@@ -5,41 +5,82 @@ import send_to_telegram
 import config
 
 
-class ZaraWomanParser:
+class ZaraParser:
     def __init__(self):
-        self.data = config.data
-        self.urls_category_id = config.urls_category_id
-        self.json_loads = dict()
-        self.v2 = ''
+        self.session = requests.Session()
 
     def main(self):
         try:
-            for url in self.make_urls():
-                self.get_data_from_json_loads(url)
+            categories = self.get_categories()
+            for section, values in categories.items():  # session -> woman, man, kid, beauty, origin
+                for category in values:
+                    for category_id, category_url in category.items():
+                        url = self.make_urls(category_id)
+                        data = self.get_data_from_json_loads(url, category_id)
+                        print(data)
         except Exception as e:
-            self.data = dict(
-                tg_channel='125975839',
-                tg_send_method='sendMessage',
-                msg_text=f'ERROR: ZARA_W - {e}'
-            )
-            send_to_telegram.main(**self.data)
-            print(e)
+            msg = f'ERROR: ZARA - {e}'
+            send_to_telegram.main(msg, 'sendMessage', True)
 
-    def make_urls(self):
-        urls = []
-        for category_id, url_category in self.urls_category_id.items():
-            urls.append(f'https://www.zara.com/kz/ru/category/{category_id}/products')
-            return urls
+    def get_categories(self) -> dict:
+        categories_ids = dict()
+        response = self.session.get(config.url_category, headers=config.headers)
+        json_loads = json.loads(response.text)
+        section_names = json_loads['categories']
+        for section_name in section_names:  # woman, man, kid, beauty, origins
+            categories_list = []
+            for category in section_name['subcategories']:
+                if 'products' in category['layout'] or 'marketing' in category['layout']:
+                    if not category['hasSubcategories']:
+                        category_name = self.formatting_category_name(category['name'])
+                        if category.get('redirectCategoryId'):
+                            category_id = category['redirectCategoryId']
+                        else:
+                            category_id = category['id']
+                        categories_list.append({category_id: category_name})
+                        continue
+                    for subcategory in category['subcategories']:
+                        if 'products' in subcategory['layout'] or 'marketing' in subcategory['layout']:
+                            if not subcategory['hasSubcategories']:
+                                category_name = self.formatting_category_name(subcategory['name'])
+                                if subcategory.get('redirectCategoryId'):
+                                    subcategory_id = subcategory['redirectCategoryId']
+                                else:
+                                    subcategory_id = subcategory['id']
+                                categories_list.append({subcategory_id: category_name})
+                                continue
+            if 'sectionName' in section_name:
+                section = section_name['sectionName']
+            else:
+                section = section_name['name']
+            categories_ids[self.formatting_category_name(section.lower())] = categories_list
+        return categories_ids
 
-    def get_data_from_json_loads(self, url):
-        session = requests.Session()
-        response = session.get(url, params=config.params, headers=config.headers)
-        self.json_loads = json.loads(response.text)
+    def formatting_category_name(self, name: str):
+        name = name.lower()
+        if 'zara' in name:
+            return name.replace('zara', '').strip()
+        if '\xa0' in name:
+            name = name.replace('\xa0', ' ')
+        if ' | ' in name:
+            return ' '.join(name.split(' | '))
+        if ' ' in name:
+            return name.replace(' ', '_')
+        if '-' in name:
+            return name.replace('-', '_')
+        return name
 
-        if len(self.json_loads['productGroups']) <= 0:
-            return
+    def make_urls(self, category_id: str) -> str:
+        return f'{config.url_product}{category_id}/products'
 
-        elements = self.json_loads['productGroups'][0]['elements']
+    def get_data_from_json_loads(self, url: str, category_id: str) -> dict:
+        response = self.session.get(url, params=config.params, headers=config.headers)
+        json_loads = json.loads(response.text)
+
+        if len(json_loads['productGroups']) <= 0:
+            return dict()
+
+        elements = json_loads['productGroups'][0]['elements']
         for element in elements:
             com_components = element.get('commercialComponents')
             if not com_components:
@@ -51,103 +92,192 @@ class ZaraWomanParser:
                 for color in colors:
                     if not color.get('name') or not color.get('price'):
                         continue
-                    self.get_category(url)
-                    # self.data['url'] = self.make_url(com_component['seo'])
-                    self.make_url(com_component['seo'])
-                    if not self.data['url']:
+                    url = self.make_url(com_component['seo'], category_id)
+                    if not url:
                         continue
-                    self.data['name'] = com_component['name']
-                    self.data['color'] = color['name']
-                    self.data['description'] = com_component.get('description', '')
-                    self.data['price'] = color['price'] // 100
-                    self.data['image'] = ', '.join(self.make_photo_urls(color['xmedia']))
-                    print(self.data)
-                    self.check_data_from_db()
+                    data = {'url': url,
+                            'name': com_component['name'],
+                            'color': color['name'],
+                            'description': com_component.get('description', ''),
+                            'price': color['price'] // 100,
+                            'image': ', '.join(self.make_photo_urls(color['xmedia'])),}
+                    return data
 
-    def get_category(self, url):
-        for category_id, category in self.urls_category_id.items():
-            if category_id in url:
-                self.v2 = category_id
-                self.data['category'] = category
-                break
-
-    def make_url(self, seo):
+    def make_url(self, seo: dict, category_id: str) -> str:
         if seo['keyword'] == '':
             return False
         keyword = f"{seo['keyword']}"
         seo_product_id = f"{seo['seoProductId']}"
         discern_product_id = f"{seo['discernProductId']}"
-        url = f"https://www.zara.com/kz/ru/{keyword}-p{seo_product_id}.html?v1={discern_product_id}&v2={self.v2}"
-        self.data['url_non_utf8'] = url
-        self.data['url'] = requests.utils.unquote(url)
-        # return url
-        # return requests.utils.unquote(url)
+        url = f"{config.url}{keyword}-p{seo_product_id}.html?v1={discern_product_id}&v2={category_id}"
+        return url
 
-    def make_photo_urls(self, photos):
+    def make_photo_urls(self, photos: list) -> list:
         photos_list = []
         for photo in photos:
             path = photo['path']
             name = photo['name']
             timestamp = photo['timestamp']
-            photo_str = f"https://static.zara.net/photos//{path}/w/750/{name}.jpg?ts={timestamp}"
+            photo_str = f"{config.url_photo}/{path}/w/750/{name}.jpg?ts={timestamp}"
             photos_list.append(photo_str)
         return photos_list
 
-    def check_data_from_db(self):
-        if working_with_db.check_product('one', **self.data) is None:
-            working_with_db.insert_data_to_products(**self.data)
-            last_inserted_row = working_with_db.last_row(**self.data)
-            self.data['product_id'] = last_inserted_row[0]
-            working_with_db.insert_data_to_db_prices(**self.data)
-        else:
-            # finded_product = [item for item in working_with_db.check_product('all', **self.data) if self.data['url'] == item]
-            finded_product = [item for item in working_with_db.check_product('all', **self.data) if
-                              self.data['url'] in item]
-            if not finded_product:
-                working_with_db.insert_data_to_products(**self.data)
-                last_inserted_row = working_with_db.last_row(**self.data)
-                self.data['product_id'] = last_inserted_row[0]
-                working_with_db.insert_data_to_db_prices(**self.data)
-            else:
-                finded_product_price = list(working_with_db.find_product_price(finded_product[0][0], **self.data))
-                self.data['product_id'] = finded_product[0][0]
-                finded_price = finded_product_price[3]
-                if finded_price != self.data['price']:
-                    self.data['discount'] = self.get_percentage(self.data['price'], finded_price)
-                    working_with_db.insert_data_to_db_prices(**self.data)
-                    # if int(self.data['discount']) <= -15 and self.data['price'] >= 3000:
-                    if int(self.data['discount']) <= -15:
-                        self.data['last_prices'] = working_with_db.last_n_prices_rows(**self.data)
-                        self.data['image_caption'] = self.make_image_caption()
-                        # self.send_to_telegram_as_image_caption()
-                        send_to_telegram.main(**self.data)
-                        print(self.data['image_caption'])
-                        # print('YES')
-
-    def get_percentage(self, price, price_old):
-        percent = round(-1 * (100 - (price * 100 / price_old)))
-        if percent > 0:
-            percent = f'+{percent}'
-        return str(percent)
-
-    def make_image_caption(self):
-        self.make_hashtag_to_category()
-        image_caption = f"<b>{self.data['name']}</b>\n" \
-                        f"<b>{self.data['color']}</b>\n" \
-                        f"#{self.data['market']} {self.data['category']}\n\n" \
-                        f"{self.data['description']}\n\n" \
-                        f"{self.data['last_prices']}\n" \
-                        f"<a href='{self.data['url_non_utf8']}'>Купить на оф.сайте</a>\n\n" \
-                        f"{self.data['tg_channel']}"
-        return image_caption
-
-    def make_hashtag_to_category(self):
-        temp_list = []
-        temp_list2 = self.data['category'].split()
-        for k in temp_list2:
-            temp_list.append(f'#{k}')
-        self.data['category'] = ' '.join(temp_list)
-
 
 if __name__ == '__main__':
-    ZaraWomanParser().main()
+    ZaraParser().main()
+
+
+# import requests
+# import json
+# import working_with_db
+# import send_to_telegram
+# import config
+#
+#
+# class ZaraWomanParser:
+#     def __init__(self):
+#         self.data = config.data
+#         self.urls_category_id = config.urls_category_id
+#         self.json_loads = dict()
+#         self.v2 = ''
+#
+#     def main(self):
+#         try:
+#             for url in self.make_urls():
+#                 self.get_data_from_json_loads(url)
+#         except Exception as e:
+#             self.data = dict(
+#                 tg_channel='125975839',
+#                 tg_send_method='sendMessage',
+#                 msg_text=f'ERROR: ZARA_W - {e}'
+#             )
+#             send_to_telegram.main(**self.data)
+#             print(e)
+#
+#     def make_urls(self):
+#         urls = []
+#         for category_id, url_category in self.urls_category_id.items():
+#             urls.append(f'https://www.zara.com/kz/ru/category/{category_id}/products')
+#         return urls
+#
+#     def get_data_from_json_loads(self, url):
+#         session = requests.Session()
+#         response = session.get(url, params=config.params, headers=config.headers)
+#         self.json_loads = json.loads(response.text)
+#
+#         if len(self.json_loads['productGroups']) <= 0:
+#             return
+#
+#         elements = self.json_loads['productGroups'][0]['elements']
+#         for element in elements:
+#             com_components = element.get('commercialComponents')
+#             if not com_components:
+#                 continue
+#             for com_component in com_components:
+#                 colors = com_component['detail']['colors']
+#                 if not com_component.get('name'):
+#                     continue
+#                 for color in colors:
+#                     if not color.get('name') or not color.get('price'):
+#                         continue
+#                     self.get_category(url)
+#                     # self.data['url'] = self.make_url(com_component['seo'])
+#                     self.make_url(com_component['seo'])
+#                     if not self.data['url']:
+#                         continue
+#                     self.data['name'] = com_component['name']
+#                     self.data['color'] = color['name']
+#                     self.data['description'] = com_component.get('description', '')
+#                     self.data['price'] = color['price'] // 100
+#                     self.data['image'] = ', '.join(self.make_photo_urls(color['xmedia']))
+#                     print(self.data)
+#                     self.check_data_from_db()
+#
+#     def get_category(self, url):
+#         for category_id, category in self.urls_category_id.items():
+#             if category_id in url:
+#                 self.v2 = category_id
+#                 self.data['category'] = category
+#                 break
+#
+#     def make_url(self, seo):
+#         if seo['keyword'] == '':
+#             return False
+#         keyword = f"{seo['keyword']}"
+#         seo_product_id = f"{seo['seoProductId']}"
+#         discern_product_id = f"{seo['discernProductId']}"
+#         url = f"https://www.zara.com/kz/ru/{keyword}-p{seo_product_id}.html?v1={discern_product_id}&v2={self.v2}"
+#         self.data['url'] = url
+#         # self.data['url_non_utf8'] = url
+#         # self.data['url'] = requests.utils.unquote(url)
+#         # return url
+#         # return requests.utils.unquote(url)
+#
+#     def make_photo_urls(self, photos):
+#         photos_list = []
+#         for photo in photos:
+#             path = photo['path']
+#             name = photo['name']
+#             timestamp = photo['timestamp']
+#             photo_str = f"https://static.zara.net/photos//{path}/w/750/{name}.jpg?ts={timestamp}"
+#             photos_list.append(photo_str)
+#         return photos_list
+#
+#     def check_data_from_db(self):
+#         if working_with_db.check_product('one', **self.data) is None:
+#             working_with_db.insert_data_to_products(**self.data)
+#             last_inserted_row = working_with_db.last_row(**self.data)
+#             self.data['product_id'] = last_inserted_row[0]
+#             working_with_db.insert_data_to_db_prices(**self.data)
+#         else:
+#             # finded_product = [item for item in working_with_db.check_product('all', **self.data) if self.data['url'] == item]
+#             finded_product = [item for item in working_with_db.check_product('all', **self.data) if
+#                               self.data['url'] in item]
+#             if not finded_product:
+#                 working_with_db.insert_data_to_products(**self.data)
+#                 last_inserted_row = working_with_db.last_row(**self.data)
+#                 self.data['product_id'] = last_inserted_row[0]
+#                 working_with_db.insert_data_to_db_prices(**self.data)
+#             else:
+#                 finded_product_price = list(working_with_db.find_product_price(finded_product[0][0], **self.data))
+#                 self.data['product_id'] = finded_product[0][0]
+#                 finded_price = finded_product_price[3]
+#                 if finded_price != self.data['price']:
+#                     self.data['discount'] = self.get_percentage(self.data['price'], finded_price)
+#                     working_with_db.insert_data_to_db_prices(**self.data)
+#                     # if int(self.data['discount']) <= -15 and self.data['price'] >= 3000:
+#                     if int(self.data['discount']) <= -15:
+#                         self.data['last_prices'] = working_with_db.last_n_prices_rows(**self.data)
+#                         self.data['image_caption'] = self.make_image_caption()
+#                         # self.send_to_telegram_as_image_caption()
+#                         send_to_telegram.main(**self.data)
+#                         print(self.data['image_caption'])
+#                         # print('YES')
+#
+#     def get_percentage(self, price, price_old):
+#         percent = round(-1 * (100 - (price * 100 / price_old)))
+#         if percent > 0:
+#             percent = f'+{percent}'
+#         return str(percent)
+#
+#     def make_image_caption(self):
+#         self.make_hashtag_to_category()
+#         image_caption = f"<b>{self.data['name']}</b>\n" \
+#                         f"<b>{self.data['color']}</b>\n" \
+#                         f"#{self.data['market']} {self.data['category']}\n\n" \
+#                         f"{self.data['description']}\n\n" \
+#                         f"{self.data['last_prices']}\n" \
+#                         f"<a href='{self.data['url']}'>Купить на оф.сайте</a>\n\n" \
+#                         f"{self.data['tg_channel']}"
+#         return image_caption
+#
+#     def make_hashtag_to_category(self):
+#         temp_list = []
+#         temp_list2 = self.data['category'].split()
+#         for k in temp_list2:
+#             temp_list.append(f'#{k}')
+#         self.data['category'] = ' '.join(temp_list)
+#
+#
+# if __name__ == '__main__':
+#     ZaraWomanParser().main()
--- send_to_telegram.py
+++ send_to_telegram.py
@@ -1,48 +1,56 @@
 import requests
 import json
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+tg_token = os.getenv('TG_TOKEN')
+url_part = f"https://api.telegram.org/bot{tg_token}/"
 
 
-def main(**kwargs):
-    if kwargs['tg_send_method'] == 'sendPhoto':
-        send_as_photo(**kwargs)
-    elif kwargs['tg_send_method'] == 'sendMediaGroup':
-        send_as_media_group(**kwargs)
+def main(msg: str, method: str, media: str, error: bool):
+    if method == 'sendMessage':
+        send_as_message(msg, error)
+    elif method == 'sendPhoto':
+        send_as_photo(msg, media)
+    elif method == 'sendMediaGroup':
+        send_as_media_group(msg, media)
 
 
-def send_as_message(**kwargs):
-    url = f'https://api.telegram.org/bot{kwargs["tg_token"]}/sendMessage'
+def send_as_message(msg: str, error: bool):
+    channel = os.getenv('TG_CHANNEL')
+    if error:
+        channel = os.getenv('TG_CHANNEL_ERROR')
+    url = f"{url_part}sendMessage"
     params = {
-        'chat_id': kwargs['tg_channel'],
-        'text': kwargs['msg_text']
+        'chat_id': channel,
+        'text': msg,
     }
     r = requests.post(url, data=params)
 
 
-def send_as_photo(**kwargs):
-    url = f'https://api.telegram.org/bot{kwargs["tg_token"]}/sendPhoto'
+def send_as_photo(msg: str, media: str):
+    url = f'{url_part}sendPhoto'
     params = {
-        'chat_id': kwargs['tg_channel'],
-        'caption': kwargs['image_caption'],
+        'chat_id': os.getenv('TG_CHANNEL'),
+        'caption': msg,
         'parse_mode': 'HTML',
-        'photo': kwargs['image'],
+        'photo': media,
     }
     r = requests.post(url, data=params)
 
 
-def send_as_media_group(**kwargs):
-    url = f'https://api.telegram.org/bot{kwargs["tg_token"]}/sendMediaGroup'
+def send_as_media_group(msg: str, media: str):
+    url = f'{url_part}sendMediaGroup'
     params = {
-        'chat_id': kwargs['tg_channel'],
+        'chat_id': os.getenv('TG_CHANNEL'),
         'media': [],
     }
-    temp_images = kwargs['image']
-    if type(kwargs['image']) == str:
-        temp_images = list(kwargs['image'].split(', '))
-        print('temp_images = STRING')
-    for path in temp_images:
+    media = list(media.split(', '))
+    for path in media:
         params['media'].append({'type': 'photo',
                                 'media': path,
                                 'parse_mode': 'HTML', })
-    params['media'][0]['caption'] = kwargs['image_caption']
+    params['media'][0]['caption'] = msg
     params['media'] = json.dumps(params['media'])
     r = requests.post(url, data=params)
