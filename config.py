1383e9fd0f86b09a52b4c41a58ea7ed18a22f4b8
daradan <daradankz@gmail.com>
2023-01-01 17:49:40 +0600

    some minor fixes

--- config.py
+++ config.py
@@ -19,20 +19,7 @@ TG_CHANNEL_ERROR = os.getenv('TG_CHANNEL_ERROR')
 PARAMS = {'ajax': 'true'}
 HEADERS = {
     'authority': 'www.zara.com',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,'
-              'application/signed-exchange;v=b3;q=0.9',
     'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-    'cache-control': 'max-age=0',
-    'if-none-match': 'W/"90e4b-fICHcllNvYuI0ZWA0xJhJSeZbnE"',
     'referer': 'https://www.zara.com/',
-    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="104", "Opera";v="90"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'same-origin',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                  'Chrome/104.0.5112.102 Safari/537.36',
+    'user-agent': os.getenv('USER_AGENT'),
 }
